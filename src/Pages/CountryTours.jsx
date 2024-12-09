5d14b874e2605e2f3121cc7cf43a6f089cb8e927
Usman Javed <usmanjaved0816@gmail.com>
2024-12-09 11:37:12 +0500

    make changes

--- src/Pages/CountryTours.jsx
+++ src/Pages/CountryTours.jsx
@@ -1,3 +1,31 @@
+
 export const CountryTours = () => {
-    return <h1>Country Tours</h1>
+    
+        // Array of country names
+        const countries = [
+          "Japan", "United States", "United Kingdom", "Iceland", "Greece", "France",
+          "Italy", "Egypt", "Australia", "Brazil", "India", "China", "Pakistan", "Bangladesh"
+        ];
+    return (
+        <>
+        <div className="bg-gradient-to-br from-green-400 to-blue-500  h-auto w-full">
+            <div className="flex flex-col justify-center items-center h-screen">
+            <div >
+                <h1 className="text-2xl text-slate-50 pb-5 ">Find inspiration for your next trip</h1>
+             </div>
+             <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 p-10 gap-10">
+      
+      {countries.map((country, index) => (
+        <button
+          key={index}
+          className="bg-gradient-to-r from-teal-200 to-lime-200 hover:from-lime-200 hover:to-teal-200 text-white font-bold py-3 px-9 rounded"
+        >
+          {country}
+        </button>
+      ))}
+    </div>
+             </div>
+        </div>
+        </>
+    )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,19 @@
+
 export const Home = () => {
 
     return (
         <>
         <div>
-        <div> 
-            <div>
-                <h1>Find Your Outdoors</h1>
-                </div>
-                div
+        <div className="bg-[url('./public/images/banner.jpg')] h-[750px] w-full bg-cover bg-center flex flex-col justify-center items-center h-screen"> 
+            <div className="text-center">
+                <div>
+                    <h1 className="text-5xl font-bold text-slate-50 "> Find Your Outdoors</h1>
+                    </div>
+                    <div className="p-2 sm:p-4 md:p-6 lg:p-8">
+                    
+                        <input  className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500  focus:border-blue-500" type="search" placeholder="Search By city, place or trail name" />
+                    </div>
+            </div>
              </div>
              </div>
         </>
